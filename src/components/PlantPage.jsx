8e7da83bf5b596f8799719840697977d1442e0bd
srj-genpact <suraj.vishwakarma@genpact.com>
2026-07-09 15:37:50 +0000

    Phase 1: Fetch and display all plants on startup

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
 
 export default PlantPage;
+
